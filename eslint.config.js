491db2c5888c4f12b36685269b717da9b7299e3a
Andrew Jefferson Mendez <andrewjeffmendez30@gmail.com>
2026-02-12 13:54:19 +0800

    chore: update project configuration and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import antfu from '@antfu/eslint-config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+export default antfu(
+	{
+		react: true,
+		stylistic: false,
+		typescript: true,
+		rules: {
+			'no-console': 'off',
+			'ts/consistent-type-definitions': 'off',
+		},
+		ignores: ['pnpm-lock.yaml', '*.md'],
+	},
+	eslintPluginPrettierRecommended,
+)
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+	tailwindStylesheet: './src/assets/main.css',
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 110,
+	endOfLine: 'auto',
+	htmlWhitespaceSensitivity: 'ignore',
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
