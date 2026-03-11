e7c56dc9d54c814eaeef2197b19626db12782b6b
Andrew Jefferson Mendez <andrewjeffmendez30@gmail.com>
2026-03-11 22:15:18 +0800

    chore: format code and update configuration files for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,15 @@ import antfu from '@antfu/eslint-config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default antfu(
-	{
-		react: true,
-		stylistic: false,
-		typescript: true,
-		rules: {
-			'no-console': 'off',
-			'ts/consistent-type-definitions': 'off',
-		},
-		ignores: ['pnpm-lock.yaml', '*.md'],
-	},
-	eslintConfigPrettier,
+  {
+    react: true,
+    stylistic: false,
+    typescript: true,
+    rules: {
+      'no-console': 'off',
+      'ts/consistent-type-definitions': 'off',
+    },
+    ignores: ['pnpm-lock.yaml', '*.md'],
+  },
+  eslintConfigPrettier,
 )
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './assets/main.css'
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
